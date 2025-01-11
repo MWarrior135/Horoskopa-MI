5583afd8c6a74f42c53db2070e1b81bbfff60439
Gundars Bremanis <gbremanis5@inbox.lv>
2025-01-11 22:40:16 +0200

    added keys, this is the first working version

--- test.py
+++ test.py
@@ -1,23 +1,15 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+load_dotenv()
 
-client = OpenAI(
-  api_key="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"
-)
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  store=True,
-  messages=[
-    {"role": "user", "content": "write a haiku about ai"}
-  ]
+    model="gpt-4o-mini",
+    store=True,
+    messages=[{"role": "user", "content": "write a haiku about ai"}],   
 )
 
-print(completion.choices[0].message);
-
-
-
- 
-#get AI API key
-#Rewatch python tutorial about jupiter notebook
+print(completion.choices[0].message.content)
